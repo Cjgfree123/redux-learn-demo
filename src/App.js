1d4339efcb3a40a9d0ca99fcc922d9389add7b79
Cjgfree123 <cjg_free@163.com>
2019-01-23 00:09:08 +0800

    进阶4:bindActionCreators(actions,dispatch)，this.props.add派发

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import * as actions from './actions/actionCreator';
+import {bindActionCreators} from 'redux'
 import './App.css';
 
 class App extends Component {
   add = () => {
-    this.props.dispatch(actions.add());
+    this.props.add();
   }
 
   render() {
@@ -18,6 +19,8 @@ class App extends Component {
   }
 }
 
-export default connect(state => {
-  return {num: state.r1}; 
-})(App);
+export default connect((state)=>{
+  return {num:state.r1};
+ },(dispatch)=>{
+   return bindActionCreators(actions,dispatch)
+ })(App);
