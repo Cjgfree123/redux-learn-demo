bf7f941702051f303ae1e2d19a8a6f425de97f46
Cjgfree123 <cjg_free@163.com>
2019-01-22 23:57:51 +0800

    进阶2:使用store.dispatch派发action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { add } from './actions/actionCreator';
 import './App.css';
 
 class App extends Component {
+  add = () => {
+    this.props.store.dispatch( add() );
+  }
+
   render() {
     return (
       <div className="App">
+        <button onClick={this.add}>增加</button>
         {this.props.store.getState().r1}
       </div>
     );
---
+++ src/actions/actionCreator.js
@@ -0,0 +1,11 @@
+import { ADD, INCREMENT } from '../type';
+export function add() {
+    return {
+        type: ADD
+    }
+}
+export function increment() {
+    return {
+        type: INCREMENT
+    }
+}
